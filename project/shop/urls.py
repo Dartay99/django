98c4464bf2612f2ef3a73e0e6d601d881351de79
dartay99@gmail.com <dartay99@gmail.com>
2023-09-24 19:20:41 +0600

    lesson_2

--- project/shop/urls.py
+++ project/shop/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from project import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -8,3 +9,7 @@ urlpatterns = [
     path('about',views.about,name='about'),
     path('contact',views.contact,name='contact'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- project/shop/views.py
+++ project/shop/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("hello world")
+    return render(request, 'shop/index.html')
 
 def help(request):
     return HttpResponse("Help is here")
